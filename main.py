dd55b43e0cd968b86f474fbf3aa47750e3ecbaba
surdeus <surdeus@gmail.com>
2023-10-18 15:27:46 +0300

    Installed the SQLAlchemy.

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI
 
+import sqlalchemy.orm as orm
+import sqlalchemy as sqla
+
+class Base(orm.DeclarativeBase):
+    pass
+
+class Question(Base):
+    __tablename__ = "questions"
+    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    text: orm.Mapped[str] = orm.mapped_column(sqla.String(256))
+    answer: orm.Mapped[str] = orm.mapped_column(sqla.String(64))
+
 app = FastAPI()
 
 @app.get("/")
