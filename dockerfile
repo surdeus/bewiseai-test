5b16d201894b77c9af9b5372da8f17d160a24d15
surdeus <surdeus@gmail.com>
2023-10-18 20:59:21 +0300

    Added automatic reloading inside docker container and implemented basic bridge to the JService API.

--- dockerfile
+++ dockerfile
@@ -1,9 +1,10 @@
 from python:3.9 as dev
 
 workdir /app
-
-copy ./ .
+copy reqs.txt .
 run pip install --no-cache-dir --upgrade -r reqs.txt
+#run pip install watchdog
 
-cmd ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
+#cmd ["watchmedo", "shell-command", "--patterns='*.py'", "--command='uvicorn main:app --host 0.0.0.0 --port 80'"]
+cmd ["uvicorn", "main:app", "--reload", "--host", "0.0.0.0", "--port", "80"]
 
--- main.py
+++ main.py
@@ -2,19 +2,39 @@ from fastapi import FastAPI
 
 import sqlalchemy.orm as orm
 import sqlalchemy as sqla
+import pydantic
+import requests
+
+class API:
+    def __init__(self, url: str):
+        self.URL = url
+    def Random(self, n: int):
+        res = requests.get(
+            self.URL + f'/random',
+            params=[
+                ('count', n),
+            ],
+        )
+        return res
+
 
 class Base(orm.DeclarativeBase):
     pass
 
-class Question(Base):
-    __tablename__ = "questions"
+class QuestionTable(Base):
+    __tablename__ = 'questions'
     id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
     text: orm.Mapped[str] = orm.mapped_column(sqla.String(256))
     answer: orm.Mapped[str] = orm.mapped_column(sqla.String(64))
 
+class RandomQuestions(pydantic.BaseModel):
+    questions_num: int
+
+api = API('https://jservice.io/api')
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello, ": "World!"}
+@app.post('/questions/')
+def read_random_questions(q: RandomQuestions):
+    res = api.Random(q.questions_num)
+    return res.content
 
